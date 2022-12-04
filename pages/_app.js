9f9f236c4693790c9ddfd7fd4898b06c67e82e1f
Robert Shepley <shepleysound@gmail.com>
2022-12-04 15:43:08 -0800

    Update content and styling.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Navbar from '../components/Navbar';
+import '../styles/globals.css';
+import Image from 'next/image';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className='flex min-h-screen flex-col'>
+        <Component {...pageProps} />
+        <footer className='flex flex-col items-center border-t border-solid border-t-slate-600 py-2'>
+          <Image
+            src='/openseattle-500x392.png'
+            alt='Open Seattle Logo'
+            width={80}
+            height={80}
+          />
+        </footer>
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
