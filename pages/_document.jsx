795aabc9c6fb3866f72dffae8982de4b1aa3af1e
Robert Shepley <shepleysound@gmail.com>
2022-12-06 18:25:08 -0800

    Add language attribute to HTML.

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -2,7 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html className='h-full bg-gray-200 dark:bg-slate-900 dark:text-white'>
+    <Html
+      lang='en-US'
+      className='h-full bg-gray-200 dark:bg-slate-900 dark:text-white'
+    >
       <Head />
       <body className='h-full'>
         <Main />
