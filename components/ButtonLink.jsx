f4e82efd7610b8739ccc4e7f6803a694d501c03d
Robert Shepley <shepleysound@gmail.com>
2022-12-06 18:23:58 -0800

    Change button color to better fit accessibility guidelines.

--- components/ButtonLink.jsx
+++ components/ButtonLink.jsx
@@ -2,7 +2,7 @@ export default function ButtonLink({ children, href }) {
   return (
     <a
       href={href || '#'}
-      className='rounded-md bg-green-600 p-2 text-sm font-bold uppercase text-white transition-colors hover:bg-green-700 active:bg-green-800'
+      className='rounded-md bg-green-700 p-2 text-sm font-bold uppercase text-white ring-white transition-colors hover:ring-1 active:bg-green-800'
     >
       {children}
     </a>
