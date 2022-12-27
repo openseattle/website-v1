cda66b14bfe340cbe46004565eb794788f93cc26
Robert Shepley <shepleysound@gmail.com>
2022-12-26 16:46:48 -0800

    Redo partner form with react-hook-form-mui.

--- pages/projects.js
+++ pages/projects.js
@@ -1,170 +1,99 @@
-import { useForm, Controller } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import { Container, Box, Stack, Button } from '@mui/material';
 import {
-  Container,
-  FormControl,
-  FormControlLabel,
-  FormLabel,
-  Radio,
-  RadioGroup,
-  Stack,
-  TextField,
-} from '@mui/material';
+  FormContainer,
+  TextFieldElement,
+  RadioButtonGroup,
+} from 'react-hook-form-mui';
 
 export default function Projects() {
-  const { handleSubmit, control, reset } = useForm({
-    defaultValues: { name: '' },
+  const formContext = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      phone: '',
+      organization: '',
+      organizationType: null,
+      website: '',
+      projectTitle: '',
+      projectDetails: '',
+    },
   });
 
-  const onSubmit = (data) => console.log(data);
+  const { reset } = formContext;
   return (
     <main>
       <h1>Projects</h1>
       <Container>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <fieldset>
-            <legend>Contact Details</legend>
-            <Stack gap={4}>
-              <Controller
-                name='name'
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <TextField label='Name' helperText='' {...field} />
-                )}
-                defaultValue=''
-              />
-              <Controller
-                name='email'
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => <TextField label='Email' {...field} />}
-                defaultValue=''
-              />
-              <Controller
-                name='phone'
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <TextField
-                    label='Phone Number'
-                    helperText='For those rare times when other communication channels break down.'
-                    {...field}
-                  />
-                )}
-                defaultValue=''
-              />
+        <FormContainer
+          onSuccess={(data) => console.log(data)}
+          formContext={formContext}
+        >
+          <Stack gap={2}>
+            <Box component='fieldset' sx={{ borderRadius: 2 }}>
+              <legend>Contact Details</legend>
+              <Stack gap={2}>
+                <TextFieldElement name='name' label='Name' required />
+                <TextFieldElement name='email' label='Email' required />
+                <TextFieldElement
+                  name='phone'
+                  label='Phone Number'
+                  helperText='For those rare times when other communication channels break down.'
+                  required
+                />
+                <TextFieldElement
+                  name='organization'
+                  label='Organization'
+                  helperText='What organization do you represent?'
+                  required
+                />
 
-              <Controller
-                name='organization'
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <TextField
-                    label='Organization'
-                    helperText='What organization do you represent?'
-                    {...field}
-                  />
-                )}
-                defaultValue=''
-              />
+                <RadioButtonGroup
+                  name='organizationType'
+                  label='Organization Type'
+                  options={[
+                    { label: 'Government Agency', id: 'government' },
+                    { label: 'Nonprofit', id: 'nonprofit' },
+                    { label: 'Community Organization', id: 'community' },
+                    { label: 'For-profit Company', id: 'for-profit' },
+                    { label: 'Other', id: 'other' },
+                  ]}
+                  row
+                  required
+                />
 
-              <Controller
-                name='organization-type'
-                control={control}
-                rules={{ required: true }}
-                defaultValue=''
-                render={({ field }) => (
-                  <FormControl>
-                    <FormLabel id='organization-type'>
-                      Organization Type
-                    </FormLabel>
-                    <RadioGroup
-                      aria-labelledby='organization-type'
-                      defaultValue=''
-                      name='organization-type'
-                      row
-                      {...field}
-                    >
-                      <FormControlLabel
-                        value='government'
-                        control={<Radio />}
-                        label='Government Agency'
-                      />
-                      <FormControlLabel
-                        value='nonprofit'
-                        control={<Radio />}
-                        label='Nonprofit'
-                      />
-                      <FormControlLabel
-                        value='community'
-                        control={<Radio />}
-                        label='Community Organization'
-                      />
-                      <FormControlLabel
-                        value='for-profit'
-                        control={<Radio />}
-                        label='For-profit Company'
-                      />
-                      <FormControlLabel
-                        value='other'
-                        control={<Radio />}
-                        label='Other'
-                      />
-                    </RadioGroup>
-                  </FormControl>
-                )}
-              />
-
-              <Controller
-                name='website'
-                control={control}
-                rules={{ required: false }}
-                render={({ field }) => (
-                  <TextField
-                    label='Website'
-                    helperText='Where can we find your organization online?'
-                    {...field}
-                  />
-                )}
-                defaultValue=''
-              />
-            </Stack>
-          </fieldset>
-          <fieldset>
-            <legend>Project</legend>
-            <Stack gap={4}>
-              <Controller
-                name='project-title'
-                control={control}
-                rules={{ required: false }}
-                render={({ field }) => (
-                  <TextField
-                    label='Project Title'
-                    helperText="What is the name of the project? It's okay if this is just a working title!"
-                    {...field}
-                  />
-                )}
-                defaultValue=''
-              />
-
-              <Controller
-                name='project-details'
-                control={control}
-                rules={{ required: false }}
-                render={({ field }) => (
-                  <TextField
-                    label='Project Details'
-                    multiline
-                    helperText='Tell us a little about the project.'
-                    {...field}
-                  />
-                )}
-                defaultValue=''
-              />
-            </Stack>
-          </fieldset>
-          <input type='submit' />
-        </form>
+                <TextFieldElement
+                  name='website'
+                  label='Website'
+                  helperText='Where can we find your organization online?'
+                />
+              </Stack>
+            </Box>
+            <Box component='fieldset' sx={{ borderRadius: 2 }}>
+              <legend>Project</legend>
+              <Stack gap={2}>
+                <TextFieldElement
+                  name='projectTitle'
+                  label='Project Title'
+                  helperText="What is the name of the project? It's okay if this is just a working title!"
+                  required
+                />
+                <TextFieldElement
+                  name='projectDetails'
+                  label='Project Details'
+                  helperText='Tell us a little about the project.'
+                  required
+                />
+              </Stack>
+            </Box>
+            <Button type='reset' variant='contained' onClick={reset}>
+              Reset
+            </Button>
+            <Button type='submit' variant='contained'>
+              Submit
+            </Button>
+          </Stack>
+        </FormContainer>
       </Container>
     </main>
   );
